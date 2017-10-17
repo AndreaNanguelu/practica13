1b7c920c1e8830a2aaf164a0a172e68d87143b86
AndreaNanguelu <30939899+AndreaNanguelu@users.noreply.github.com>
2017-10-17 17:29:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="procesar.php" method="post" name="datospersonales">
+  Nombre:<br>
+  <input type="text" name="NombreAlumno" value="" placeholder="Introduce tu nombre">
+  <br>
+  Apellidos:<br>
+  <input type="text" name="ApellidosAlumno" value="" placeholder="Introduce tu apellido">
+  <br>
+  Edad:<br>
+  <input type="text" name="EdadAlumno" value=""placeholder="Introduce tu edad">
+  <br>
+  <input type="submit" value="Guardar">
+</form> 
+
+<p>Andrea Lizeth Nanguelu Alcocer</p>
+
+</body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,8 @@
+<?php
+$nombre=$_POST['NombreAlumno'];
+$apellidos=$_POST['ApellidosAlumno'];
+$edad=$_POST['EdadAlumno'];
+echo 'TU NOMBRE ES:' .$nombre.'     '.$apellidos;
+echo '<br>';
+echo 'TU EDAD ES:'.$edad;
+?>
